1c626e82e3b68421b00a1606cd33cb00fa3b94b5
V.Kharitonovitch <morainrallez@gmail.com>
2018-02-19 22:34:56 +0300

    warmup2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function warmup(temperature) {
 const celsius = temperature ;
 let fahrenheit = celsius * (9 / 5) + 32 ;
-Math.floor(fahrenheit);
+fahrenheit = Math.floor(fahrenheit);
 return fahrenheit;
 };
